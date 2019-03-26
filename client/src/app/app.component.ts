947e4670efe9f2176dd21f98030e97820340a4db
Mr. Bishnu Prasad Khanal <bkhanal1@my.centennialcollege.ca>
2019-03-26 00:29:22 -0400

    angular header and footer added

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'comp308-w2019-lesson8b';
+}
